11766b79e6d63884620e7aa7dfa9222edcacf2af
Ryan McGee <ramtech51@comcast.net>
2021-03-07 16:42:22 -0500

    Initial commit

---
+++ main/main.c
@@ -0,0 +1,26 @@
+/* Hello World Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include <unistd.h>
+#include "esp_system.h"
+
+
+void app_main(void)
+{
+    
+    while(true)
+    {
+
+        usleep(1000 * 10);
+    }
+
+    printf("Restarting now.\n");
+    fflush(stdout);
+    esp_restart();
+}
